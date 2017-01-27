ac463c8c639dc80ad8b7af4e7b19cc404cdb29b5
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2017-01-27 11:17:02 -0500

    add ingredient type and ingredient distinction

--- cornucopia-backend/api/models.py
+++ cornucopia-backend/api/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Ingredient(models.Model):
+class IngredientType(models.Model):
     MEASURE_TYPES = (
             ('W', 'WEIGHT'),
             ('V', 'VOLUME'),
@@ -14,3 +14,11 @@ class Ingredient(models.Model):
     estimated_shelf_life = models.DurationField()
     preferred_measure_type = models.CharField(choices=MEASURE_TYPES)
     density = models.FloatField() # kg/m^3
+
+class Ingredient(models.Model):
+    ingredient_type = models.ForeignKey('IngredientType')
+    quantity = models.FloatField() # units change based on measure
+    measure = models.CharField(max_length=64)
+    expiration_date = models.DateField()
+    is_expiration_estimated = models.BooleanField()
+
