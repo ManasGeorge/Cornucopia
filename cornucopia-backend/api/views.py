6ee46a087f85d1aaaff579615e8ad4e740c53797
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2017-01-27 13:53:37 -0500

    set up suggestions api endpoint

--- cornucopia-backend/api/views.py
+++ cornucopia-backend/api/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Cornucopia Backend API")
+    return HttpResponse("Cornucopia Backend API")
+
+def suggest_ingredient(request, **kwargs):
+    prefix = kwargs['prefix']
+    # do something
+    return HttpResponse(prefix)
