e6d311fb91a55392a8a7d6a64e82cddaaa143a7a
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2017-02-09 10:45:59 -0500

    working get_ingredient_type_by_id

--- cornucopia-backend/api/urls.py
+++ cornucopia-backend/api/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^suggest_ingredient/(?P<prefix>[a-z A-Z]+)$', views.suggest_ingredient, name='suggest_ingredient'),
+    url(r'^ingredient/suggest/(?P<prefix>[a-z A-Z]+)$', views.suggest_ingredient, name='suggest_ingredient'),
+    url(r'^ingredient/by_id/(?P<id>[0-9]+)$', views.by_id, name='by_id'),
+    url(r'^recipe/can_make/$', views.can_make_recipes, name='can_make_recipes'),
+    url(r'^recipe/could_make/$', views.could_make_recipes, name='could_make_recipes'),
+    url(r'^recipe/browse/$', views.browse_recipes, name='browse_recipes'),
 ]
--- cornucopia-backend/api/views.py
+++ cornucopia-backend/api/views.py
@@ -2,7 +2,7 @@ from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from api.models import IngredientType
+from api.models import Recipe, IngredientType
 from api.utils import initialize_suggestions
 
 
@@ -13,9 +13,30 @@ def index(request):
     return HttpResponse("Cornucopia Backend API")
 
 def suggest_ingredient(request, **kwargs):
+    """Returns a list of suggested ingredient types, based on a query string"""
     global suggest
     if suggest is None:
         suggest = initialize_suggestions()
     prefix = kwargs['prefix']
     data = suggest(prefix)
     return JsonResponse(data, safe=False)
+
+def by_id(request, **kwargs):
+    """Returns an ingredient type based on its id"""
+    ingredient = model_to_dict(IngredientType.objects.get(pk=kwargs['id']))
+    return JsonResponse(ingredient)
+
+def can_make_recipes(request):
+    """Returns recipes the user can make, given their ingredients"""
+    # TODO(irapha): use ingredients in the POST request to filter and order
+    pass
+
+def could_make_recipes(request):
+    """Returns recipes the user could make, with their ingredients plus a few"""
+    # TODO(irapha): use ingredients in the POST request to filter and order
+    pass
+
+def browse_recipes(request):
+    """Returns recipes recommended to the user given their id"""
+    # TODO(irapha): use user_id in POST request to retrieve favorites and use rec system
+    pass
