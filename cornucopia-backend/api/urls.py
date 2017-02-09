2e2ba766749b22604b029e448766b8146018076f
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2017-02-09 11:05:37 -0500

    full recipe retrieval api and initial recipe suggestions done

--- cornucopia-backend/api/urls.py
+++ cornucopia-backend/api/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^ingredient/suggest/(?P<prefix>[a-z A-Z]+)$', views.suggest_ingredient, name='suggest_ingredient'),
-    url(r'^ingredient/by_id/(?P<id>[0-9]+)$', views.by_id, name='by_id'),
-    url(r'^recipe/can_make/$', views.can_make_recipes, name='can_make_recipes'),
-    url(r'^recipe/could_make/$', views.could_make_recipes, name='could_make_recipes'),
-    url(r'^recipe/browse/$', views.browse_recipes, name='browse_recipes'),
+    url(r'^ingredient/by_id/(?P<id>[0-9]+)$', views.ingredient_by_id, name='ingredient_by_id'),
+    url(r'^recipe/by_id/(?P<id>[0-9]+)$', views.recipe_by_id, name='recipe_by_id'),
+    url(r'^recipe/suggest/can_make/$', views.can_make_recipes, name='can_make_recipes'),
+    url(r'^recipe/suggest/could_make/$', views.could_make_recipes, name='could_make_recipes'),
+    url(r'^recipe/suggest/browse/$', views.browse_recipes, name='browse_recipes'),
 ]
