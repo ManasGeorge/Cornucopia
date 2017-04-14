8e5ead9899a32cf5479481d54bc0fe945c04511d
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2017-04-14 15:59:52 -0400

    oops

--- cornucopia-backend/api/utils/csp.py
+++ cornucopia-backend/api/utils/csp.py
@@ -87,6 +87,7 @@ class RecipeSuggester(object):
 
             # sort recipes
             can_make_recipes = [i[0] for i in sorted(can_make_recipes, key=lambda x: x[1])]
+            raise Exception(could_make_recipes)
             could_make_recipes = [i[0] for i in sorted(could_make_recipes, key=lambda x: x[1])]
 
         return can_make_recipes, could_make_recipes
